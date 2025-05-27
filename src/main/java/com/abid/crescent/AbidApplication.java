96b813f9192e67f3dfb6dda4be9df019f6780b6c
Abid Ali <mohdabidaliusa@gmail.com>
2025-05-26 21:25:27 -0700

    Dynamically Injected Dependency.

--- src/main/java/com/abid/crescent/AbidApplication.java
+++ src/main/java/com/abid/crescent/AbidApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AbidApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(AbidApplication.class, args);
     }
 
--- src/main/java/com/abid/crescent/config/AppConfig.java
+++ src/main/java/com/abid/crescent/config/AppConfig.java
@@ -7,6 +7,30 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
+
+    /*
+        Dynamically initialized bean
+        (Unsatisfied dependency problem)
+
+        Suppose there is an interface Order which has 2 implementations
+        OnlineOrder and OfflineOrder
+
+        Suppose we are creating a bean User and injecting Order in it.
+
+        This creates an unsatisfied dependency problem.
+        {Meaning Spring boot not does not know which order to pick OnlineOrder or OfflineOrder}
+
+        To solve this we have 3 ways:
+        1. @Primary on top of any order OnlineOrder or OfflineOrder
+        2. @Qualifier("name") on top of Bean and on top of @Autowired.
+
+        Using any of these two breaks the Dependency Inversion Principle.
+
+        A better way to do the same thing would be to inject dependency dynamically
+        using @Value when creating dependency.
+
+     */
+
     /*
        required a bean of type 'java.lang.String' that could not be found
        Consider defining a bean of type 'java.lang.String' in your configuration.
@@ -15,5 +39,7 @@ public class AppConfig {
 //    public User createUserBean(){
 //        return new User("defaultFirstName", "defaultLastName");
 //    }
+
+
 
 }
